4952ffa250b90d36f589a88cd84d8dfb923dd993
vbresan <288855+vbresan@users.noreply.github.com>
2022-10-28 10:23:54 +0200

    Dodavanje novog klijenta

---
+++ docs/za-gabrijelu/reducers.js
@@ -0,0 +1,40 @@
+import { UPDATE_VALUE, CLEAR_FORM, LOAD_NALOG } from 'actions'
+
+const _defaultState = {
+    nalog: {
+        naziv: '',
+        platitelj__ime: '',
+        platitelj__adresa: '',
+        platitelj__gradMjesto: '',
+        primatelj__ime: 'Zaklada Solidarna',
+        primatelj__adresa: ' ',
+        primatelj__gradMjesto: ' ',
+        primatelj__iban: 'HR6324020061500078383',
+        iznos: '',
+        primatelj__model: '',
+        primatelj__pozivNaBroj: '11-2022',
+        sifra__namjene: '',
+        opis_placanja: 'Donacija za Gabrijelu'
+    }
+};
+
+const nalog = (state, action) => {
+    switch(action.type) {
+        case UPDATE_VALUE:
+            return(Object.assign({}, state, { [action.id]:action.value }));
+        case CLEAR_FORM:
+            return(Object.assign({}, state, _defaultState.nalog));
+        case LOAD_NALOG:
+            return(Object.assign({}, state, action.nalog));
+    }
+
+    return(state);
+}
+
+const reducer = (state=_defaultState, action) => {
+    return({
+        nalog: nalog(state.nalog, action)
+    })
+}
+
+export { reducer }
